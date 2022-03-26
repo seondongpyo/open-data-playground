fff5e64aa1c536669105ee8f1cf56b4230c5dcc9
seondongpyo <seondongpyo@gmail.com>
2022-03-26 23:16:30 +0900

    Initial commit

---
+++ src/test/java/io/github/seondongpyo/OpenDataPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.seondongpyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenDataPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
